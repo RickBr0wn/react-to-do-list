948deb06cc048fd5d1fe31d44541caa9be621a26
RiCK BROWN <ricky.brown.00@gmail.com>
2018-05-05 21:12:14 +0100

    Added Title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import Title from './Components/Title'
+
 
 class App extends Component {
   render() {
+    const styles = {
+      display: 'flex'
+    }
+
     return (
-      <div className="App">
-        
+      <div className="container" style={styles}>
+        <Title />
       </div>
     );
   }
---
+++ src/Components/Title.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const styles = {
+
+}
+
+const Title = () => {
+  return(
+    <div style={styles}>
+      TITLE
+    </div>
+  )
+}
+
+export default Title
